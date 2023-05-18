3b8d222079d476ba9834ec33d1b66d91c30096be
Samuel Vestur <samuel.vestur@karatnet.com>
2023-05-18 16:23:38 +0200

    FV + comments

---
+++ parserEset/InfectedFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parserEset
+{
+    internal class InfectedFile
+    {
+        public string Name { get; set; }
+        public string Archiver { get; set; }
+        public string Packer { get; set; }
+        public string Threat { get; set; }
+        public string Info { get; set; }    
+    }
+}
--- parserEset/Program.cs
+++ parserEset/Program.cs
@@ -1,33 +1,39 @@
-﻿using System;
+﻿using parserEset;
+using System;
 using System.Collections.Generic;
 using System.IO;
-
-class InfectedFile
-{
-    public string Name { get; set; }
-    public string Archiver { get; set; }
-    public string Packer { get; set; }
-    public string Threat { get; set; }
-   
-}
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Paste the path to the source file: (for example - C:\\My Stuff\\Programovanie\\Zadanie\\ESET_Parser attachment.log)");
+        Console.WriteLine("A parser that identifies infected files.");
+        Console.WriteLine();
+        Console.WriteLine("Insert the path to the source file with the extension .log or .txt. \n(For example = C:\\My Stuff\\Programovanie\\Zadanie\\ESET_Parser attachment.log)");
         string filePath = Console.ReadLine();
 
+        /*
+            vytvorenie premennej infectedFiles typu List<InfectedFile>, metóda ParseInfectedFiles je volaná argumentom filePath 
+            čo je cesta k vstupnému súboru
+        */
         List<InfectedFile> infectedFiles = ParseInfectedFiles(filePath);
 
+        //volanie metod PrintInfectedFiles() a PrintSummary()
         PrintInfectedFiles(infectedFiles);
         PrintSummary(infectedFiles);
 
         Console.ReadKey();
     }
 
+
+    /*
+        Táto metóda "ParseInfectedFiles" vykoná spracovanie vstupného súboru s cestou filePath 
+        a vytvára zoznam objektov typu "InfectedFile". Metóda otvára vstupný súbor pomocou 
+        "StreamReader" a postupne spracovava každý riadok v súbore. 
+     */
     static List<InfectedFile> ParseInfectedFiles(string filePath)
     {
+        //vytvorenie objektu typu List<InfectedFile>
         List<InfectedFile> infectedFiles = new List<InfectedFile>();
 
         // Načítanie obsahu vstupného súboru
@@ -36,7 +42,7 @@ class Program
             while (!reader.EndOfStream)
             {
                 string line = reader.ReadLine();
-
+                // overenie pomocou metódy "StartsWith()" či daný retazec začína danou hodnotou "name="
                 if (line.StartsWith("name="))
                 {
                     // Parsovanie riadku a extrahovanie informácií
@@ -44,6 +50,7 @@ class Program
                     string threat = GetValueFromLine(line, "threat=");
                     string archiver = GetArchiver(name);
                     string packer = GetPacker(name);
+                    string info = GetValueFromLine(line, "info=");
 
                     // Vytvorenie objektu InfectedFile a pridanie do zoznamu
                     InfectedFile infectedFile = new InfectedFile
@@ -51,7 +58,8 @@ class Program
                         Name = name,
                         Threat = threat,
                         Archiver = archiver,
-                        Packer = packer
+                        Packer = packer,
+                        Info = info
                     };
 
                     infectedFiles.Add(infectedFile);
@@ -62,19 +70,72 @@ class Program
         return infectedFiles;
     }
 
+    /*
+        Metóda PrintInfectedFiles slúži na výpis informácií o infikovaných súborov 
+        na konzolu. Metóda prechádza každý objekt InfectedFile v zozname infectedFiles pomocou cyklu foreach.
+        Pre každý infikovaný súbor, ktorý nie je označený ako "is OK", sa vykoná mnou navrhnuté zobrazenie 
+        pre infikované súbory. 
+     */
+
+    static void PrintInfectedFiles(List<InfectedFile> infectedFiles)
+    {
+        Console.WriteLine("\nInfected Files:");
+        int count = 1;
+        foreach (InfectedFile file in infectedFiles)
+        {
+            if (file.Threat != "is OK")
+            {
+                string fileName = RemoveArchiverFromFileName(file.Name);
+                string archive = RemoveArchiverFromThreat(file.Archiver);
+                string packer = RemovePackerFromFileName(file.Name);
+                string info = file.Info;
+
+                Console.Write($"[{count}] File path: {fileName} | Threat: {file.Threat}" +
+                                  (!string.IsNullOrEmpty(archive) ? $" | Archive: {archive}" : "") +
+                                  (!string.IsNullOrEmpty(packer) ? $" | Packer: {packer}" : ""));
+
+                if (!string.IsNullOrEmpty(info))
+                {
+                    Console.Write($" | Info: {info}");
+                }
+
+                Console.WriteLine();
+                count++;
+            }
+        }
+    }
+
+    /*
+        Metóda GetValueFromLine() slúži na získanie hodnoty zo zadaného riadku na základe kľúča.
+     */
     static string GetValueFromLine(string line, string key)
     {
         int startIndex = line.IndexOf(key) + key.Length;
         int endIndex = line.IndexOf(",", startIndex);
 
+        // -1 znamená, že žiadna čiarka sa nenaška, nastaví sa index konca na koniec reťazca 
         if (endIndex == -1)
         {
             endIndex = line.Length;
         }
-
+        /*
+            operácia Szbstring, ktorá vytvorí podretazec v riadku.
+            Tento podretazec je od indexu zaciatku po koniec a tým 
+            dostaneme samotnu hodnotu. Pomocou metódy Trim odstranime 
+            uvodzovky.
+         */
         return line.Substring(startIndex, endIndex - startIndex).Trim('\"');
     }
 
+    /*
+       Metóda slúži na získanie informácií o archíve. Parametrom je fileName čo je reťazec obsahujúci názov súboru.
+     
+       Rozdelí vstupný súbor na viacero častí pomocou metódy Split, kde oddeľovacím reťazcom
+       je tento symbol " » ". Ak existuje aspoň jedna časť tak pomocou metódy Replace()
+       sa nahradí za prazdny string. 
+
+        Ak sa v názve súboru nevyskytuje sekcia archivátora, vráti sa prázdny reťazec
+     */
     static string GetArchiver(string fileName)
     {
         string[] parts = fileName.Split(" » ");
@@ -101,6 +162,7 @@ class Program
         }
     }
 
+    // Metóda slúžiaca na vypísanie informácií o počte daných súborov. 
     static void PrintSummary(List<InfectedFile> infectedFiles)
     {
         int totalCount = infectedFiles.Count;
@@ -112,25 +174,8 @@ class Program
         Console.WriteLine($"Non-infected Files: {okCount}");
     }
 
-    static void PrintInfectedFiles(List<InfectedFile> infectedFiles)
-    {
-        Console.WriteLine("\nInfected Files:");
-        int count = 1;
-        foreach (InfectedFile file in infectedFiles)
-        {
-            if (file.Threat != "is OK")
-            {
-                string fileName = RemoveArchiverFromFileName(file.Name);
-                string archive = RemoveArchiverFromThreat(file.Archiver);
-                string packer = RemovePackerFromFileName(file.Name);
 
-                Console.WriteLine($"[{count}] File: {fileName} | Threat: {file.Threat}" +
-                                  (!string.IsNullOrEmpty(archive) ? $" | Archive: {archive}" : "") +
-                                  (!string.IsNullOrEmpty(packer) ? $" | Packer: {packer}" : ""));
-                count++;
-            }
-        }
-    }
+    // Metódy slúžiace na odstránenie.
 
     static string RemoveArchiverFromFileName(string fileName)
     {
